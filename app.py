abbdbebd8248371302a23ff8656168c9425df6d3
Joe deBlaquiere <jadb@Mac-Mini.local>
2016-03-18 10:11:54 -0400

    updated to use tornado

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-#!/usr/bin/env python
-from flask import Flask, render_template, send_from_directory, send_file, request, abort, jsonify
-from msgfile import Message
-import os
 import time
+import tornado.escape
+import tornado.ioloop
+import tornado.web
+import tornado.options
+import logging
+import os
 import mmap
-
-app = Flask(__name__)
+from msgfile import Message
 
 config={}
 config['receive_dir'] = "recv/"
@@ -16,123 +17,154 @@ config['header_size'] = (8+1+8+1+66+1+66+1+66)
 
 messagelist = []
 
-@app.route('/css/<path:path>')
-def send_css(path):
-    return send_from_directory('static/css',path)
+class TimeHandler(tornado.web.RequestHandler):
+    def get(self):
+        response = { 'time' : int(time.time()) }
+        self.write(response)
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html', messagelist=messagelist)
+class VersionHandler(tornado.web.RequestHandler):
+    def get(self):
+        response = { 'version': '0.0.1' }
+        self.write(response)
 
-@app.route('/api/message/upload', methods=['POST'])
-def receive_message_raw():
-    filedata = request.files['message']
-    recvpath = config['receive_dir'] + str(int(time.time() * 100))
-    #app.logger.debug('receiving file as ' + recvpath )
-    filedata.save(recvpath)
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("templates/index.html", messagelist=messagelist)
+
+class MessageUploadHandler(tornado.web.RequestHandler):
+    def post(self):
+        filereq = self.request
+        filedata = self.request.files['message'][0]
+        print('filereq =', filereq)
+        print('filedata =', str(filedata))
+        recvpath = config['receive_dir'] + str(int(time.time() * 1000))
+        logging.info('receiving file as ' + recvpath )
+        fh = open(recvpath, 'wb')
+        fh.write(filedata['body'])
+        fh.close()
     
-    with open(recvpath,'rb') as f :
-        mm = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
-        header = mm[:config['header_size']]
-        mm.close()
-    t = header.split(':')[0]
-    e = header.split(':')[1]
-    I = header.split(':')[2]
-    J = header.split(':')[3]
-    K = header.split(':')[4]
+        with open(recvpath,'rb') as f :
+            mm = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
+            header = mm[:config['header_size']]
+            mm.close()
+        t = header.split(':')[0]
+        e = header.split(':')[1]
+        I = header.split(':')[2]
+        J = header.split(':')[3]
+        K = header.split(':')[4]
 
-    #app.logger.debug ('checking for dup ' + I)
-    #todo: handle duplicate hash
-    for l in messagelist:
-        if l.time == int(t,16):
-            if l.expire == int(e,16):
-                if l.I == I:
-                    if l.J == J:
-                        if l.K == K:
-                            app.logger.debug ('dup detected')
-                            os.remove(recvpath)
-                            return jsonify(l.metadata())
+        #app.logger.debug ('checking for dup ' + I)
+        #todo: handle duplicate hash
+        for l in messagelist:
+            if l.time == int(t,16):
+                if l.expire == int(e,16):
+                    if l.I == I:
+                        if l.J == J:
+                            if l.K == K:
+                                logging.info ('dup detected')
+                                os.remove(recvpath)
+                                self.write(l.metadata())
         
-    #app.logger.debug ('no dup ' + I)
-    
-    m = Message()
-    if m.ingest(recvpath,I) != True :
-        app.logger.debug('ingest failed for message ' + I)
-        os.remove(recvpath)
-        abort(400)
+        #app.logger.debug ('no dup ' + I)
 
-    msgpath = config['message_dir'] + I
-    m.move_to(msgpath)
-    messagelist.insert(0,m)
-    return jsonify(m.metadata())
+        m = Message()
+        if m.ingest(recvpath,I) != True :
+            app.logger.debug('ingest failed for message ' + I)
+            os.remove(recvpath)
+            abort(400)
 
-@app.route('/api/message/download/<I>', methods=['GET'])
-def send_message_raw(I):
-    for m in messagelist :
-        if m.I == I :
-            return send_file(m.get_file())
-    else :
-        abort(404)
+        msgpath = config['message_dir'] + I
+        m.move_to(msgpath)
+        messagelist.insert(0,m)
+        self.write(m.metadata())
 
-@app.route('/api/message/find/<I>', methods=['GET'])
-def find_shard_data(I):
-    for m in messagelist:
-        if m.I == I :
-            return jsonify(m.metadata())
-    else :
-        abort(404)
-
-@app.route('/api/time', methods=['GET'])
-def server_time():
-    r = {}
-    r['time'] = int(time.time())
-    return jsonify(r)
-
-@app.route('/api/message/list', methods=['GET'])
-def list_messages():
-    l = []
-    now = int(time.time())
-    for m in messagelist:
-        if m.expire < now:
-            app.logger.debug('deleting expired message ' + m.I)
-            messagelist.remove(m)
-            m.delete()
-        else:
-            l.append(m.metadata())
-    #print 'l=', l
-    ml = { "message_list" : l }
-    return jsonify(ml)
-
-@app.route('/api/message/list/since/<T>', methods=['GET'])
-def list_messages_since(T):
-    l = []
-    now = int(time.time())
-    for m in messagelist:
-        if m.expire < now:
-            app.logger.debug('deleting expired message ' + m.I)
-            messagelist.remove(m)
-            m.delete()
-        else:
-            if m.servertime > int(T):
+class MessageListHandler(tornado.web.RequestHandler):
+    def get(self):
+        l = []
+        now = int(time.time())
+        for m in messagelist:
+            if m.expire < now:
+                app.logger.debug('deleting expired message ' + m.I)
+                messagelist.remove(m)
+                m.delete()
+            else:
                 l.append(m.metadata())
-    #print 'l=', l
-    ml = { "message_list" : l }
-    return jsonify(ml)
+        #print 'l=', l
+        ml = { "message_list" : l }
+        self.write(ml)
 
-@app.route('/api/status', methods=['GET'])
-def send_status():
-    used = 0
-    for m in messagelist:
-        used += m.size
-    status = {}
-    storage = {}
-    storage["capacity"] = config["capacity"]
-    storage["used"] = used
-    storage["max_file_size"] = config["max_file_size"]
-    storage['messages'] = len(messagelist)
-    status["storage"] = storage
-    return jsonify(status)
 
+class MessageListSinceHandler(tornado.web.RequestHandler):
+    def get(self, time_id=None):
+        l = []
+        now = int(time.time())
+        for m in messagelist:
+            if m.expire < now:
+                app.logger.debug('deleting expired message ' + m.I)
+                messagelist.remove(m)
+                m.delete()
+            else:
+                if m.servertime > int(time_id):
+                    l.append(m.metadata())
+        #print 'l=', l
+        ml = { "message_list" : l }
+        self.write(ml)
+
+class StatusHandler(tornado.web.RequestHandler):
+    def get(self):
+        used = 0
+        for m in messagelist:
+            used += m.size
+        status = {}
+        storage = {}
+        storage["capacity"] = config["capacity"]
+        storage["used"] = used
+        storage["max_file_size"] = config["max_file_size"]
+        storage['messages'] = len(messagelist)
+        status["storage"] = storage
+        self.write(status)
+
+class MessageDownloadHandler(tornado.web.RequestHandler):
+    def get(self, msg_id=None):
+        logging.info('download hash ' + str(msg_id))
+        for m in messagelist :
+            if m.I == msg_id :
+                with m.get_file() as f:
+                    self.set_header('Content-Type','application/octet-stream' )
+                    while 1:
+                        data = f.read(16384) # or some other nice-sized chunk
+                        if not data: break
+                        self.write(data)
+                    self.finish()
+                    return
+        else :
+            self.set_status(404)
+            self.finish()
+
+class MessageFindHandler(tornado.web.RequestHandler):
+    def get(self, msg_id=None):
+        for m in messagelist:
+            if m.I == msg_id :
+                self.write(m.metadata())
+                return
+        else :
+            self.set_status(404)
+            self.finish()
+
+application = tornado.web.Application([
+    (r'/static/(.*)/?', tornado.web.StaticFileHandler, {'path':'static'}),
+    (r'/api/message/download/(?P<msg_id>[0-9a-fA-F]+$)/?', MessageDownloadHandler),
+    (r'/api/message/find/(?P<msg_id>[0-9a-fA-F]+$)/?', MessageFindHandler),
+    (r'/api/message/upload/?', MessageUploadHandler),
+    (r'/api/message/list/?', MessageListHandler),
+    (r'/api/message/list/since/(?P<time_id>\d+$)/?', MessageListSinceHandler),
+    (r'/api/status/?', StatusHandler),
+    (r'/api/time/?', TimeHandler),
+    (r'/api/version/?', VersionHandler),
+    (r'/?', IndexHandler),
+    (r'/index.html', IndexHandler)
+])
+ 
 def rescan_inventory():
     filenames = os.listdir(config['message_dir'])
     now = int(time.time())
@@ -145,16 +177,18 @@ def rescan_inventory():
                     break
             else:
                 if msg.expire > now:
-                    print 'adding', fname
+                    logging.info('adding ' + fname)
                     messagelist.append(msg)
                 else:
-                    print 'expired', fname
+                    logging.info('expired ' + fname)
                     os.remove(filepath)
     messagelist.sort(key=lambda msg: msg.expire, reverse=True)
 
-if __name__ == '__main__' :
-    #app.run(host="0.0.0.0", debug=True, port=5000, ssl_context='adhoc')
-    print 'scanning message inventory'
+if __name__ == "__main__":
+    tornado.options.parse_command_line()
+    logging.info('msgstore started unix time ' + str(int(time.time())))
+    logging.info('scanning message inventory')
     rescan_inventory()
-    app.run(host="0.0.0.0", debug=True)
-    
+    logging.info('imported %d messages' % len(messagelist))
+    application.listen(5555)
+    tornado.ioloop.IOLoop.instance().start()
