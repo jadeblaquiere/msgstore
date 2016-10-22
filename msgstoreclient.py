da5a3ad213e0f98373cdf9f0f5b10ad327b46fd2
jadeblaquiere <jadeblaquiere@yahoo.com>
2016-10-21 21:31:24 -0400

    Revert "use ciprtxt-lib implementation of MsgStore client"
    This reverts commit 92828f0f10276926f1cdb57fb2d2c97fd6e1e937.

---
+++ msgstoreclient.py
@@ -0,0 +1,198 @@
+# Copyright (c) 2016, Joseph deBlaquiere <jadeblaquiere@yahoo.com>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of ciphrtxt nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+from ciphrtxt.message import RawMessageHeader, Message
+import requests
+import requests_futures
+from requests_futures.sessions import FuturesSession
+from requests.exceptions import Timeout, ConnectionError, HTTPError
+import time
+import json
+import io
+from threading import Lock
+
+_baseurl = 'http://ciphrtxt.com:5000/'
+
+_server_time = 'api/time/'
+_headers_since = 'api/header/list/since/'
+_download_message = 'api/message/download/'
+_upload_message = 'api/message/upload/'
+
+_cache_expire_time = 5 # seconds
+_high_water = 50
+_low_water = 20
+
+_default_timeout = 10 # seconds
+
+class MsgStore (object):
+    """Client library for message store server"""
+    def __init__(self,baseurl=_baseurl):
+        self.baseurl = baseurl
+        self.headers = []
+        self.cache_dirty = True
+        self.last_sync = time.time()
+        self.servertime = 0
+        self.session = requests.session()
+        self.futures_session = FuturesSession(session=self.session)
+        self._get_queue = []
+        self._post_queue = []
+        self._insert_lock = Lock()
+        self._gq_lock = Lock()
+
+    def _sync_headers(self):
+        now = time.time()
+        if not self.cache_dirty:
+            delay = now - self.last_sync
+            if (now - self.last_sync) < _cache_expire_time:
+                return True
+        #print('request headers from ' + self.baseurl)
+        r = None
+        try:
+            r = requests.get(self.baseurl + _server_time, timeout = _default_timeout)
+        except (Timeout, ConnectionError, HTTPError):
+            return False
+        if r.status_code != 200:
+            return False
+        servertime = json.loads(r.text)['time']
+        for h in self.headers:
+            if servertime > h.expire:
+                self._insert_lock.acquire()
+                #print('expiring ' + h.Iraw().decode())
+                self.headers.remove(h)
+                self._insert_lock.release()
+        self.last_sync = time.time()
+        try:
+            r = self.session.get(self.baseurl + _headers_since + str(self.servertime))
+        except (Timeout, ConnectionError, HTTPError):
+            return False
+        if r.status_code != 200:
+            return False
+        self.servertime = servertime
+        self.cache_dirty = False
+        #remote = sorted(json.loads(r.text)['header_list'],
+        #                key=lambda k: int(k[6:14],16), reverse=True)
+        remote = json.loads(r.text)['header_list']
+        for rstr in reversed(remote):
+            rhdr = RawMessageHeader()
+            if rhdr._deserialize_header(rstr.encode()):
+                self._insert_lock.acquire()
+                if rhdr not in self.headers:
+                    self.headers.insert(0, rhdr)
+                self._insert_lock.release()
+        self._insert_lock.acquire()
+        self.headers.sort(reverse=True)
+        self._insert_lock.release()
+        return True
+    
+    def get_headers(self):
+        self._sync_headers()
+        return self.headers
+
+    def get_message(self, hdr, callback=None):
+        self._sync_headers()
+        if hdr not in self.headers:
+            return None
+        r = None
+        try:
+            r = self.session.get(self.baseurl + _download_message + hdr.Iraw().decode(), stream=True)
+        except (Timeout, ConnectionError, HTTPError):
+            return None
+        if r.status_code != 200:
+            return None
+        raw = b''
+        for chunk in r:
+            raw += chunk
+        msg = Message.deserialize(raw)
+        if callback:
+            return callback(msg)
+        return msg
+
+    def _cb_get_async(self, s, r):
+        qen = [qe for qe in self._get_queue if qe[0] == r.url]
+        cb = qen[0][1]
+        self._gq_lock.acquire()
+        self._get_queue.remove(qen[0])
+        self._gq_lock.release()
+        if cb is None:
+            return None
+        if r.status_code != 200:
+            print('Async Reply Error ' + str(r.status_code) + ' ' + r.url)
+            return cb(None)
+        msg = Message.deserialize(r.text.encode())
+        return cb(msg)
+
+    def get_message_async(self, hdr, callback):
+        self._sync_headers()
+        if hdr not in self.headers:
+            return False
+        url = self.baseurl + _download_message + hdr.Iraw().decode()
+        qentry = (url, callback)
+        if qentry in self._get_queue:
+            return False
+        if len(self._get_queue) > _high_water:
+            while len(self._get_queue) > _low_water:
+                time.sleep(1)
+        self._gq_lock.acquire()
+        self._get_queue.append(qentry)
+        self._gq_lock.release()
+        r = self.futures_session.get(url, background_callback=self._cb_get_async)
+        return True
+    
+    def post_message(self, msg):
+        if msg in self.headers:
+            return
+        raw = msg.serialize()
+        nhdr = RawMessageHeader.deserialize(raw)
+        f = io.StringIO(raw.decode())
+        files = {'message': ('message', f)}
+        try:
+            r = self.session.post(self.baseurl + _upload_message, files=files)
+        except (Timeout, ConnectionError, HTTPError):
+            return
+        if r.status_code != 200:
+            return
+        self._insert_lock.acquire()
+        if nhdr not in self.headers:
+            self.headers.insert(0,nhdr)
+        self._insert_lock.release()
+        self.cache_dirty = True
+
+
+if __name__ == '__main__':  # pragma: no cover
+    mstore = MsgStore()
+    for h in mstore.get_headers():
+        msg = mstore.get_message(h)
+        print(msg)
+        print()
+    while True:
+        time.sleep(1)
+        mstore._sync_headers()
+
+                
+                
+            
---
+++ onion_test.py
@@ -0,0 +1,208 @@
+# Copyright (c) 2016, Joseph deBlaquiere <jadeblaquiere@yahoo.com>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of ciphrtxt nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import requests
+import hashlib
+import base64
+import json
+import sys
+from binascii import hexlify, unhexlify
+import nak
+
+from ecpy.curves import curve_secp256k1
+from ecpy.point import Point, Generator
+from ecpy.ecdsa import ECDSA
+from Crypto.Random import random
+from Crypto.Cipher import AES
+from Crypto.Util import Counter
+
+Point.set_curve(curve_secp256k1)
+_G = Generator.init(curve_secp256k1['G'][0], curve_secp256k1['G'][1])
+ECDSA.set_generator(_G)
+
+client_p = random.randint(1,curve_secp256k1['n']-1)
+client_P = _G * client_p
+
+client_r = random.randint(1,curve_secp256k1['n']-1)
+client_R = _G * client_r
+
+ecdsa=ECDSA()
+
+_server = "http://indigo.bounceme.net:5000/"
+_server2 = "http://coopr8.com:5000/"
+_server3 = "http://ciphrtxt.com:5000/"
+_status = "api/status/"
+_onion = "onion/"
+_nak_priv = 0xf1a91fc566427a45cd6cdd43f5fc5647b1d6696a5b03f868b9bb8b01b631ae91
+
+nak = nak.NAK(expire = 0, privkey = _nak_priv)
+print('nak = %s' % str(nak))
+print('nak_pubkey = %s' % nak.pubkey.compress())
+nakpubbin = unhexlify(nak.pubkey.compress())
+
+r = requests.get(_server + _status)
+assert r.status_code == 200
+rd = r.json()
+
+server_P = Point.decompress(rd['pubkey'])
+server_R = server_P
+
+o_r = {}
+o_r['local'] = True
+o_r['url'] = 'api/status/'
+o_r['action'] = 'get'
+o_r['replykey'] = client_R.compress()
+
+message = json.dumps(o_r)
+
+ecdh = server_P * client_p
+keybin = hashlib.sha256(ecdh.compress().encode('UTF-8')).digest()
+iv = random.randint(0,(1 << 128)-1)
+print('iv = 0x%032x' % iv)
+ivbin = unhexlify('%032x' % iv)
+counter = Counter.new(128, initial_value=iv)
+cryptor = AES.new(keybin, AES.MODE_CTR, counter=counter)
+ciphertext = cryptor.encrypt(message)
+raw = ivbin + ciphertext
+sig = nak.sign(raw)
+signed = nakpubbin + unhexlify('%064x' % sig[0]) + unhexlify('%064x' % sig[1]) + raw
+payload = base64.b64encode(signed)
+
+r = requests.post(_server + _onion + client_P.compress(), data=payload)
+assert r.status_code == 200
+d_bd = base64.b64decode(r.text)
+sig = (int(hexlify(d_bd[0:32]),16), int(hexlify(d_bd[32:64]),16))
+print('sig = ' + str(sig))
+assert ecdsa.verify(server_R, sig, d_bd[64:])
+d_ecdh = server_R * client_r
+d_keybin = hashlib.sha256(d_ecdh.compress().encode('UTF-8')).digest()
+print('ecdh key hex = ' + str(hexlify(d_keybin)))
+d_ivcount = int(hexlify(d_bd[64:80]),16)
+print('iv hex = ' + str(hexlify(d_bd[64:80])))
+d_counter = Counter.new(128,initial_value=d_ivcount)
+d_cryptor = AES.new(d_keybin, AES.MODE_CTR, counter=d_counter)
+d_plaintext = d_cryptor.decrypt(d_bd[80:])
+d_plaintext = d_plaintext.decode('UTF-8')
+print("plaintext = " + d_plaintext)
+
+r = requests.get(_server2 + _status)
+assert r.status_code == 200
+rd = r.json()
+
+server_P2 = Point.decompress(rd['pubkey'])
+
+oo_r = {}
+oo_r['local'] = False
+oo_r['host'] = 'indigo.bounceme.net'
+oo_r['port'] = 5000
+oo_r['pubkey'] = client_P.compress()
+oo_r['body'] = base64.b64encode(raw).decode('UTF-8')
+
+client_p = random.randint(1,curve_secp256k1['n']-1)
+client_P = _G * client_p
+
+message = json.dumps(oo_r)
+
+ecdh = server_P2 * client_p
+keybin = hashlib.sha256(ecdh.compress().encode('UTF-8')).digest()
+iv = random.randint(0,(1 << 128)-1)
+print('iv = 0x%032x' % iv)
+ivbin = unhexlify('%032x' % iv)
+counter = Counter.new(128, initial_value=iv)
+cryptor = AES.new(keybin, AES.MODE_CTR, counter=counter)
+ciphertext = cryptor.encrypt(message)
+raw = ivbin + ciphertext
+sig = nak.sign(raw)
+signed = nakpubbin + unhexlify('%064x' % sig[0]) + unhexlify('%064x' % sig[1]) + raw
+payload = base64.b64encode(signed)
+
+r = requests.post(_server2 + _onion + client_P.compress(), data=payload)
+assert r.status_code == 200
+d_bd = base64.b64decode(r.text)
+sig = (int(hexlify(d_bd[0:32]),16), int(hexlify(d_bd[32:64]),16))
+print('sig = ' + str(sig))
+assert ecdsa.verify(server_R, sig, d_bd[64:])
+d_ecdh = server_R * client_r
+d_keybin = hashlib.sha256(d_ecdh.compress().encode('UTF-8')).digest()
+print('ecdh key hex = ' + str(hexlify(d_keybin)))
+d_ivcount = int(hexlify(d_bd[64:80]),16)
+print('iv hex = ' + str(hexlify(d_bd[64:80])))
+d_counter = Counter.new(128,initial_value=d_ivcount)
+d_cryptor = AES.new(d_keybin, AES.MODE_CTR, counter=d_counter)
+d_plaintext = d_cryptor.decrypt(d_bd[80:])
+d_plaintext = d_plaintext.decode('UTF-8')
+print("plaintext = " + d_plaintext)
+
+r = requests.get(_server3 + _status)
+assert r.status_code == 200
+rd = r.json()
+
+server_P3 = Point.decompress(rd['pubkey'])
+
+ooo_r = {}
+ooo_r['local'] = False
+ooo_r['host'] = 'coopr8.com'
+ooo_r['port'] = 5000
+ooo_r['pubkey'] = client_P.compress()
+ooo_r['body'] = base64.b64encode(raw).decode('UTF-8')
+
+client_p = random.randint(1,curve_secp256k1['n']-1)
+client_P = _G * client_p
+
+message = json.dumps(ooo_r)
+
+ecdh = server_P3 * client_p
+keybin = hashlib.sha256(ecdh.compress().encode('UTF-8')).digest()
+iv = random.randint(0,(1 << 128)-1)
+print('iv = 0x%032x' % iv)
+ivbin = unhexlify('%032x' % iv)
+counter = Counter.new(128, initial_value=iv)
+cryptor = AES.new(keybin, AES.MODE_CTR, counter=counter)
+ciphertext = cryptor.encrypt(message)
+raw = ivbin + ciphertext
+sig = nak.sign(raw)
+signed = nakpubbin + unhexlify('%064x' % sig[0]) + unhexlify('%064x' % sig[1]) + raw
+payload = base64.b64encode(signed)
+
+r = requests.post(_server3 + _onion + client_P.compress(), data=payload)
+assert r.status_code == 200
+d_bd = base64.b64decode(r.text)
+sig = (int(hexlify(d_bd[0:32]),16), int(hexlify(d_bd[32:64]),16))
+print('sig = ' + str(sig))
+assert ecdsa.verify(server_R, sig, d_bd[64:])
+d_ecdh = server_R * client_r
+d_keybin = hashlib.sha256(d_ecdh.compress().encode('UTF-8')).digest()
+print('ecdh key hex = ' + str(hexlify(d_keybin)))
+d_ivcount = int(hexlify(d_bd[64:80]),16)
+print('iv hex = ' + str(hexlify(d_bd[64:80])))
+d_counter = Counter.new(128,initial_value=d_ivcount)
+d_cryptor = AES.new(d_keybin, AES.MODE_CTR, counter=d_counter)
+d_plaintext = d_cryptor.decrypt(d_bd[80:])
+d_plaintext = d_plaintext.decode('UTF-8')
+print("plaintext = " + d_plaintext)
+
+
